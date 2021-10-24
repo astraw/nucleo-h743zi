e1d2ecab7ae1203b6842b4a046b0b7e7b43f0db7
Robin Mueller <robin.mueller.m@gmail.com>
2021-10-24 14:27:30 +0200

    update comments

--- src/bin/blinky.rs
+++ src/bin/blinky.rs
@@ -22,13 +22,14 @@ fn main() -> ! {
     let dp = stm32h7xx_hal::stm32::Peripherals::take().unwrap();
     let cp = cortex_m::Peripherals::take().unwrap();
 
-    // Take ownership over the raw flash and rcc devices and convert them into the corresponding
-    // HAL structs
+    // Take ownership over the RCC devices and convert them into the corresponding HAL structs
     let rcc = dp.RCC.constrain();
 
     let pwr = dp.PWR.constrain();
     let pwrcfg = pwr.freeze();
 
+    // Freeze the configuration of all the clocks in the system and
+    // retrieve the Core Clock Distribution and Reset (CCDR) object
     let rcc = rcc.use_hse(8.mhz()).bypass_hse();
     let ccdr = rcc.freeze(pwrcfg, &dp.SYSCFG);
 
